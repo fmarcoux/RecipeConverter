2cab549772dd11ecd561e8b6a81f92a44ed13967
francois marcoux <marf2910@usherbrooke.ca>
2023-09-09 18:05:59 -0400

    WIP disk text saver

---
+++ ReceipeConverter/ReceipeConverter/src/Classe/CDiskSaver.cs
@@ -0,0 +1,63 @@
+﻿using ReceipeConverter.src.Interface;
+using System.Text;
+
+
+namespace ReceipeConverter.src.Classe
+{
+    internal class CDiskSaver : ITextSaver
+    {
+        private readonly DirectoryInfo m_destinationRepository ;
+
+        public CDiskSaver(string repositoryPath)
+        {
+            if (!IsValidRepository(repositoryPath))
+            {
+                m_destinationRepository = Directory.CreateDirectory(repositoryPath);
+            }
+            else
+            {
+                //Le directory existe déjà
+                m_destinationRepository = new DirectoryInfo(repositoryPath);
+            }
+             
+        }
+
+        public bool Save(IEnumerable<string> text)
+        {
+            if (!text.Any<string>()) return false;
+            return Save(String.Join(" ", text), text.First());
+        }
+
+        public bool Save(string wholeText, string fileName)
+        {
+            if (String.IsNullOrEmpty(wholeText)) return false;
+            FileStream f = new FileStream($"{m_destinationRepository.FullName}{Path.PathSeparator}{fileName}", FileMode.Create);
+            f.Write(Encoding.UTF8.GetBytes(wholeText));
+            return true;
+        }
+
+        public int SaveMultiple(IReadOnlyList<IReadOnlyList<string>> multipleText)
+        {
+            int failCounter = 0;
+            foreach (var text in multipleText)
+            {
+                try
+                {
+                    Save(text);
+                }
+                catch { failCounter++; }
+            }
+            return failCounter;
+        }
+
+        /// <summary>
+        /// Validate if a repository exists
+        /// </summary>
+        /// <param name="repositoryPath">the repository to validate</param>
+        /// <returns></returns>
+        private bool IsValidRepository(string repositoryPath)
+        {
+            return Directory.Exists(repositoryPath); 
+        }
+    }
+}
---
+++ ReceipeConverter/ReceipeConverter/src/Interface/ITextSaver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceipeConverter.src.Interface
+{
+    internal interface ITextSaver
+    {
+        /// <summary>
+        /// Saves the strings in a single entity
+        /// </summary>
+        /// <param name="text">List of string to save</param>
+        /// <returns></returns>
+        public bool Save(IEnumerable<string> text);
+
+
+        /// <summary>
+        /// Saves the string in a single entity
+        /// </summary>
+        /// <param name="wholeText"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public bool Save(string wholeText,string fileName);
+
+        /// <summary>
+        /// Saves the data in multiple entities
+        /// </summary>
+        /// <param name="multipleText"> List containing Lists of data to be saved</param>
+        /// <returns>the number of entities that have not been saved successfully</returns>
+        public int SaveMultiple(IReadOnlyList<IReadOnlyList<string>> multipleText);
+        //TODO : Améliorer le error handling pour cette fonction, il serait probablement intéressant de savoir la/lesquelles on fail
+    }
+}
